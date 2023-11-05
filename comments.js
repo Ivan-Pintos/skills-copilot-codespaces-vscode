b985ba7c9a4b611a086bbc21527bab6f7173a23c
IvanPintos <114030309+Ivan-Pintos@users.noreply.github.com>
2023-11-05 02:37:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,41 @@
-//Create a web server
-//Create a route
-//Get the data from the database
-//Send the data back to the client
+// Create web server
 
-//Create a web server
+// Import express
+const express = require('express');
+const router = express.Router();
+
+// Import comment controller
+const commentsController = require('../controllers/commentsController');
+
+// Import auth controller
+const authController = require('../controllers/authController');
+
+// Import comment validation
+const commentValidation = require('../validations/commentValidation');
+
+// Import comment validation
+const commentUpdateValidation = require('../validations/commentUpdateValidation');
+
+// Import comment validation
+const commentDeleteValidation = require('../validations/commentDeleteValidation');
+
+// Import comment validation
+const commentGetValidation = require('../validations/commentGetValidation');
+
+// Get all comments
+router.get('/', commentsController.getAllComments);
+
+// Get all comments
+router.get('/:id', commentGetValidation.validate('getComment'), commentsController.getComment);
+
+// Create new comment
+router.post('/', authController.verifyToken, commentValidation.validate('createComment'), commentsController.createComment);
+
+// Update comment
+router.put('/:id', authController.verifyToken, commentUpdateValidation.validate('updateComment'), commentsController.updateComment);
+
+// Delete comment
+router.delete('/:id', authController.verifyToken, commentDeleteValidation.validate('deleteComment'), commentsController.deleteComment);
+
+// Export module
+module.exports = router;
