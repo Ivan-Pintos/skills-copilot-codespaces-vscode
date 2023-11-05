49cea7f8cfe4a9878004d0179963fb7c103afdf1
IvanPintos <114030309+Ivan-Pintos@users.noreply.github.com>
2023-11-05 02:30:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,41 @@
 //Create a web server
+//Create a route
+//Get the data from the database
+//Send the data back to the client
 
+//Create a web server
 const express = require('express');
 const app = express();
-const path = require('path');
+const cors = require('cors');
 const port = 3000;
 
-// Set up the static directory to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set up the route for the comments form
-app.post('/comments', (req, res) => {
-  res.send('POST request to the homepage');
+//Create a route
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
 
-// Start the server
+//Get the data from the database
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+//Send the data back to the client
+app.get('/comments', (req, res) => {
+  pool.query(`
+  SELECT students.id, students.name, cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  LIMIT 5;
+  `)
+  .then(result => {
+    res.json(result.rows);
+  })
+  .catch(err => console.error('query error', err.stack));
+});
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening on port ${port}!`);
 });
